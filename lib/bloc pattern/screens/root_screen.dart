68f01a07af74426dd25e913ec34c12eecc708117
PatrykWysocki5140 <estu@onet.pl>
2022-10-03 20:36:43 +0200

    bloc dark mode and navi bar

---
+++ lib/bloc pattern/screens/root_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fueler/pages/settings_page.dart';
+
+import '../bloc/navbar/navigation_cubit.dart';
+import '../bloc/theme/theme_cubit.dart';
+import '../elements/nav_bar_items.dart';
+import 'settings_screen.dart';
+
+class RootScreen extends StatefulWidget {
+  @override
+  _RootScreenState createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flueler'),
+        actions: [
+          BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return Switch(
+                  activeColor: Colors.black,
+                  value: state.isDarkThemeOn,
+                  onChanged: (newValue) {
+                    context.read<ThemeCubit>().toggleSwitch(newValue);
+                  });
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BlocBuilder<NavigationCubit, NavigationState>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            currentIndex: state.index,
+            showUnselectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings,
+                ),
+                label: 'Settings',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                ),
+                label: 'Profile',
+              ),
+            ],
+            onTap: (index) {
+              if (index == 0) {
+                BlocProvider.of<NavigationCubit>(context)
+                    .getNavBarItem(NavbarItem.home);
+              } else if (index == 1) {
+                BlocProvider.of<NavigationCubit>(context)
+                    .getNavBarItem(NavbarItem.settings);
+              } else if (index == 2) {
+                BlocProvider.of<NavigationCubit>(context)
+                    .getNavBarItem(NavbarItem.profile);
+              }
+            },
+          );
+        },
+      ),
+      body: BlocBuilder<NavigationCubit, NavigationState>(
+          builder: (context, state) {
+        if (state.navbarItem == NavbarItem.home) {
+          return Text("1"); //HomeScreen();
+        } else if (state.navbarItem == NavbarItem.settings) {
+          return SettingsScreen();
+        } else if (state.navbarItem == NavbarItem.profile) {
+          return Text("3"); //ProfileScreen();
+        }
+        return Container();
+      }),
+    );
+  }
+}
