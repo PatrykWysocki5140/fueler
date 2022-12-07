5834e236287a511726102fcf9725fa6ab5619f6e
PatrykWysocki5140 <estu@onet.pl>
2022-12-07 12:55:20 +0100

    map page

--- lib/routes/tabs/map_page.dart
+++ lib/routes/tabs/map_page.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bart/bart.dart';
 
-class MapPage extends StatelessWidget with AppBarNotifier {
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
   final BuildContext parentContext;
   const MapPage({
     Key? key,
@@ -10,10 +15,85 @@ class MapPage extends StatelessWidget with AppBarNotifier {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Text("map page");
+  // ignore: no_logic_in_create_state
+  _MapPage createState() => _MapPage(parentContext);
+}
+
+class _MapPage extends State<MapPage> with AppBarNotifier {
+  _MapPage(BuildContext parentContext);
+
+  BuildContext get parentContext => parentContext;
+
+  late GoogleMapController mapController;
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+
+    updateAppBar(
+      context,
+      AppBar(
+        title: const Text('Flueler'),
+        leading: FloatingActionButton(
+          heroTag: "/inner",
+          child: const Icon(Icons.refresh),
+          onPressed: () {
+            // Navigator.of(parentContext).pushNamed("/parent");
+            Navigator.of(context).pushNamed("/profile/inner");
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: FloatingActionButton(
+                heroTag: "/settingsss",
+                child: const Icon(Icons.refresh),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/profile/inner");
+                },
+              )),
+        ],
+      ),
+    );
+    showAppBar(context);
   }
-  /*
+
+  @override
+  Widget build(BuildContext context) {
+    //return Text("data");
+    _getCurrentLocation();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            _currentPosition.latitude,
+            _currentPosition.longitude,
+          ),
+          zoom: 14.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+      ),
+    );
+  }
+
+  void _getCurrentLocation() async {
+    //Geolocator geolocator = await Geolocator();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+        forceAndroidLocationManager: true);
+    //Position position = await Geolocator.
+    setState(() {
+      _currentPosition = position;
+    });
+  }
+}
+
+/*
     return Center(
       child: SingleChildScrollView(
         child: Column(
@@ -77,5 +157,6 @@ class MapPage extends StatelessWidget with AppBarNotifier {
         ),
       ),
     );
-  }*/
+  }
 }
+*/
