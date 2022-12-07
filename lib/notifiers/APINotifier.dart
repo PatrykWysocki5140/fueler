30f560d8cf2c902271570b0d3f8d96e77ba8b99c
PatrykWysocki5140 <estu@onet.pl>
2022-12-07 12:55:04 +0100

    login user

--- lib/notifiers/APINotifier.dart
+++ lib/notifiers/APINotifier.dart
@@ -1,8 +1,11 @@
 // ignore: file_names
+import 'dart:convert';
 import 'dart:developer' as dev;
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
 import 'package:fueler/model/API_Model/User.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,13 +13,35 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../model/API_Model/Api_service.dart';
 
 class Api with ChangeNotifier {
-  List<User> users = List.empty();
+  String baseUrl = "http://fueler.data.awrzawinski.xyz";
+  String preferencesKeyToken = "token";
+  String preferencesKeyUser = "userData";
   User user = User();
+
+  ////
+  List<User> users = List.empty();
   ApiService api = ApiService();
   bool loading = false;
   bool savedUser = false;
+  String? token = null;
 
   // ignore: non_constant_identifier_names
+  Future<dynamic> GetLocalUser() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final jsonString = prefs.getString(preferencesKeyUser);
+
+    if (jsonString == null) {
+      if (user.id != null) {
+        user.Clear();
+      }
+    } else {
+      log("SharedPreferences save user: " + jsonString);
+      user = User.fromJson(jsonString);
+    }
+  }
+
+  /*
   Future<dynamic> GetLocalUser() async {
     dynamic val;
     SharedPreferences.getInstance().then((prefs) async {
@@ -41,7 +66,7 @@ class Api with ChangeNotifier {
       if (user.id != null) SaveLocalUser(user);
     });
   }
-
+*/
   // ignore: non_constant_identifier_names
   void SaveLocalUser(User u) {
     //users.add(u);
@@ -71,10 +96,26 @@ class Api with ChangeNotifier {
 
   Future<bool> LogOut() async {
     user.Clear();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(preferencesKeyUser);
+    prefs.remove(preferencesKeyToken);
+    return true;
+  }
+
+  /*
+  Future<bool> LogOut() async {
+    user.Clear();
     SharedPreferences.getInstance().then((prefs) {
       prefs.setString("UserID", "null");
     });
     notifyListeners();
+    return true;
+  }
+*/
+  Future<bool> DeleteAllPreferences() async {
+    user.Clear();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
     return true;
   }
 
@@ -98,5 +139,94 @@ class Api with ChangeNotifier {
     log("UpdateUser wyjscie " + _user!.toJson().toString());
 
     return _user;
+  }
+
+/////////////////////////////////////////////////////////////////////
+
+  Future<User?> notifierLogInUser(String login, String password) async {
+    User? user;
+    token = (await api.getUserToken(login, password));
+    user = (await api.getLoginUserData(token!));
+    if ((token!.isNotEmpty) && (user != null)) {
+      SharedPreferences.getInstance().then((prefs) {
+        prefs.setString(preferencesKeyToken, token!);
+        prefs.setString(preferencesKeyUser, user!.toJson().toString());
+      });
+      notifyListeners();
+    }
+    return user;
+  }
+
+  Future<User?> login(String username, String password) async {
+    // Utwórz obiekt Dio z konfiguracją
+    var dio = Dio();
+    User _user = User();
+
+    // Utwórz adres URL do logowania użytkownika z parametrami zapytania
+    var url = '$baseUrl/api/users/login?name=$username&password=$password';
+
+    log(url);
+    try {
+      // Wyślij żądanie HTTP GET do adresu URL
+      var response = await dio.get(url);
+
+      // Pobierz token z odpowiedzi
+      var token = response.data;
+      log("token:" + token);
+      // Zapisz token w SharedPreferences
+      var prefs = await SharedPreferences.getInstance();
+      await prefs.setString(preferencesKeyToken, token);
+
+      // Jeśli status 200 i token różny od null
+      if ((response.statusCode == 200) && (token != null)) {
+        Future<String?> _userFromJson = getMyData();
+        // ignore: unrelated_type_equality_checks
+        if (_userFromJson != "") {
+          _user = await User.fromJson(await _userFromJson);
+          await prefs.setString(preferencesKeyUser, _user.toJson().toString());
+          user = _user;
+          return _user;
+        }
+        //Zwracanie pobranego usera
+      }
+    } catch (e) {
+      // Jeśli wystąpi błąd, zwróć pustego usera
+      return _user;
+    }
+  }
+
+  Future<String?> getMyData() async {
+    // Pobierz token z SharedPreferences
+    var prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString(preferencesKeyToken);
+
+    // Utwórz obiekt Dio z konfiguracją
+    var dio = Dio();
+
+    // Utwórz adres URL do pobierania własnych danych użytkownika
+    String url = '$baseUrl/api/users/me';
+    log("wejście: " + url);
+    Response response;
+    try {
+      // Wyślij żądanie HTTP GET do adresu URL z nagłówkiem autoryzacyjnym
+      response = await dio.get(
+        url,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+    } on DioError catch (e) {
+      // Jeśli wystąpi błąd, zwróć pusty
+      log("e.response!.data: " + e.response!.data.toString());
+      return "";
+    }
+    log("status:" + response.statusCode.toString());
+    // Pobierz dane użytkownika z odpowiedzi
+    if (response.statusCode == 200) {
+      log("response.data: " + response.data.toString());
+      return await response.data.toString();
+    } else {
+      log("empty");
+      // Jeśli status inny niż 200, zwróć pusty
+      return "";
+    }
   }
 }
