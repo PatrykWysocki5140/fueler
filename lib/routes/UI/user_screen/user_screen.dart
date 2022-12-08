b3720ea754635110451d432a20266a431738f76e
PatrykWysocki5140 <estu@onet.pl>
2022-12-08 15:39:10 +0100

    add delete user

--- lib/routes/UI/user_screen/user_screen.dart
+++ lib/routes/UI/user_screen/user_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
 import 'dart:ui';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:fueler/routes/UI/user_screen/widgets/deleteUser_dialog.dart';
 import 'package:fueler/routes/UI/user_screen/widgets/logout_dialog.dart';
 import 'package:fueler/settings/Get_colors.dart';
 
@@ -14,7 +17,9 @@ import '../../../settings/validator.dart';
 
 class UserScreen extends StatefulWidget {
   static String id = "user_screen";
-  const UserScreen({Key? key}) : super(key: key);
+  const UserScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<UserScreen> createState() => _UserScreenState();
@@ -26,6 +31,8 @@ class _UserScreenState extends State<UserScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController newemailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController currentpasswordController =
+      TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController userPrivilegeLevelController =
       TextEditingController();
@@ -33,7 +40,7 @@ class _UserScreenState extends State<UserScreen> {
   final TextEditingController firstpasswordController = TextEditingController();
   final TextEditingController secondpasswordController =
       TextEditingController();
-  //final ApiClient _apiClient = Provider.of<Api>(context, listen: true);
+
   bool _loadUserData = true;
   bool _showPassword = true;
   bool _newPassword = false;
@@ -41,23 +48,21 @@ class _UserScreenState extends State<UserScreen> {
 
   Future<void> UpdatePassword() async {
     if (_formKey.currentState!.validate()) {
-      User _newUserPassword = Provider.of<Api>(context, listen: false).user;
-      passwordController.text = firstpasswordController.text;
-
-      _newUserPassword.SetNewPassword(passwordController.text);
-      User? _user = await Provider.of<Api>(context, listen: false)
-          .UpdateUser(_newUserPassword, context);
-
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(AppLocalizations.of(context)!.procesing),
-        backgroundColor: GetColors.success,
+        backgroundColor: GetColors.warning,
       ));
+      Response? _response = await Provider.of<Api>(context, listen: false)
+          .updatePassword(currentpasswordController.text,
+              firstpasswordController.text, context);
+
       ScaffoldMessenger.of(context).hideCurrentSnackBar();
 
-      if (_user?.password == _newUserPassword.password) {
+      if (_response?.statusCode == 204) {
         _showPassword = false;
         firstpasswordController.clear();
         secondpasswordController.clear();
+        currentpasswordController.clear();
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text(AppLocalizations.of(context)!.success),
           backgroundColor: GetColors.success,
@@ -73,20 +78,17 @@ class _UserScreenState extends State<UserScreen> {
 
   Future<void> UpdateEmail() async {
     if (_formKey.currentState!.validate()) {
-      User _newUserEmail = Provider.of<Api>(context, listen: false).user;
-      emailController.text = newemailController.text;
-
-      _newUserEmail.SetNewEmail(emailController.text);
-      User? _user = await Provider.of<Api>(context, listen: false)
-          .UpdateUser(_newUserEmail, context);
-
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(AppLocalizations.of(context)!.procesing),
-        backgroundColor: GetColors.success,
+        backgroundColor: GetColors.warning,
       ));
+      emailController.text = newemailController.text;
+      Response? _response = await Provider.of<Api>(context, listen: false)
+          .updateUserData(nameController.text, phoneNumberController.text,
+              emailController.text, context);
       ScaffoldMessenger.of(context).hideCurrentSnackBar();
 
-      if (_user?.email == _newUserEmail.email) {
+      if (_response?.statusCode == 204) {
         _newEmail = false;
         newemailController.clear();
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -118,6 +120,7 @@ class _UserScreenState extends State<UserScreen> {
     }
 
     var size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Form(
         key: _formKey,
@@ -276,7 +279,7 @@ class _UserScreenState extends State<UserScreen> {
                       readOnly: true,
                       enableInteractiveSelection: false,
                       focusNode: FocusNode(),
-                      obscureText: _showPassword,
+                      obscureText: true,
                       validator: (value) =>
                           Validator.validatePassword(value ?? "", context),
                       controller: passwordController,
@@ -303,6 +306,17 @@ class _UserScreenState extends State<UserScreen> {
                       Container(
                           child: Column(
                         children: [
+                          SizedBox(height: size.height * 0.01),
+                          TextFormField(
+                            obscureText: _showPassword,
+                            controller: currentpasswordController,
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(
+                              hintText:
+                                  AppLocalizations.of(context)!.currentpassword,
+                              isDense: true,
+                            ),
+                          ),
                           SizedBox(height: size.height * 0.01),
                           TextFormField(
                             obscureText: _showPassword,
@@ -341,19 +355,28 @@ class _UserScreenState extends State<UserScreen> {
                                           (firstpasswordController
                                                       .text.isNotEmpty &&
                                                   secondpasswordController
+                                                      .text.isNotEmpty &&
+                                                  currentpasswordController
                                                       .text.isNotEmpty)
                                               ? GetColors.red
                                               : GetColors.gray)),
                               onPressed: () {
                                 if (firstpasswordController.text.isNotEmpty &&
-                                    secondpasswordController.text.isNotEmpty) {
+                                    secondpasswordController.text.isNotEmpty &&
+                                    currentpasswordController.text.isNotEmpty) {
                                   UpdatePassword();
                                 } else {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                       SnackBar(
                                           content: Text(
-                                              AppLocalizations.of(context)!
-                                                  .setnewpassword),
+                                              currentpasswordController
+                                                      .text.isNotEmpty
+                                                  ? AppLocalizations.of(
+                                                          context)!
+                                                      .setnewpassword
+                                                  : AppLocalizations.of(
+                                                          context)!
+                                                      .setcurrentpassword),
                                           backgroundColor: GetColors.warning));
                                 }
                               },
@@ -379,12 +402,54 @@ class _UserScreenState extends State<UserScreen> {
                                 backgroundColor:
                                     MaterialStatePropertyAll<Color>(
                                         GetColors.red)),
-                            onPressed: () => showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) =>
-                                    const LogOutDialog()),
+                            onPressed: (() async {
+                              bool? val = await showDialog<bool>(
+                                  context: context,
+                                  builder: (BuildContext context) =>
+                                      const LogOutDialog());
+                              if (val == true) {
+                                Navigator.of(context).pushNamed("/profile");
+                              }
+                            }),
                             child: Text(
                               AppLocalizations.of(context)!.logout,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: size.height * 0.01),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          //width: double.infinity,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                        GetColors.red)),
+                            onPressed: (() async {
+                              bool? val = await showDialog<bool>(
+                                  context: context,
+                                  builder: (BuildContext context) =>
+                                      const DeleteUserDialog());
+                              if (val == true) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(AppLocalizations.of(context)!
+                                      .deleteaccountfinish),
+                                  backgroundColor: GetColors.success,
+                                ));
+                                Navigator.of(context).pushNamed("/profile");
+                              }
+                            }),
+                            child: Text(
+                              AppLocalizations.of(context)!.deleteaccount,
                               style: const TextStyle(
                                 fontSize: 20,
                                 fontWeight: FontWeight.bold,
