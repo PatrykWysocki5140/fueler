7662765b09d1a1f3a3a0860b6f3ea6d346a95c9d
PatrykWysocki5140 <estu@onet.pl>
2022-12-10 16:18:03 +0100

    create user view

---
+++ lib/routes/UI/user_admin_screen/widgets/create_user_screen.dart
@@ -0,0 +1,218 @@
+import 'dart:ui';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:fueler/settings/Get_colors.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../model/API_Model/User.dart';
+import '../../../../model/API_Model/UserPrivilegeLevel.dart';
+import '../../../../notifiers/APINotifier.dart';
+import '../../../../settings/validator.dart';
+
+class CreateUserScreen extends StatefulWidget {
+  static String id = "register_screen";
+
+  const CreateUserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateUserScreen> createState() => _CreateUserScreenState();
+}
+
+class _CreateUserScreenState extends State<CreateUserScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController firstpasswordController = TextEditingController();
+  final TextEditingController secondpasswordController =
+      TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  //final ApiClient _apiClient = Provider.of<Api>(context, listen: true);
+  bool _showPassword = true;
+
+  Future<void> registerUsers() async {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(AppLocalizations.of(context)!.procesing),
+        backgroundColor: GetColors.warning, //Colors.green.shade300,
+      ));
+      Response? _response = await Provider.of<Api>(context, listen: false)
+          .registerUser(nameController.text, phoneController.text,
+              emailController.text, firstpasswordController.text);
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+      if (_response?.statusCode == 204) {
+        //Navigator.push(context,MaterialPageRoute(builder: (context) => const LoginScreen()));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('${AppLocalizations.of(context)!.success}'),
+          backgroundColor: GetColors.success, //Colors.red.shade300,
+        ));
+        Navigator.of(context).pushNamed("/profile");
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('${AppLocalizations.of(context)!.error}'),
+          backgroundColor: GetColors.error, //Colors.red.shade300,
+        ));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      //backgroundColor: Colors.blueGrey[200],
+      body: Form(
+        key: _formKey,
+        child: SizedBox(
+          width: size.width,
+          height: size.height,
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: size.width * 0.85,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: size.height * 0.08),
+                    Center(
+                      child: Text(
+                        AppLocalizations.of(context)!.createnewuser,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.05),
+                    SizedBox(height: size.height * 0.03),
+                    TextFormField(
+                      validator: (value) =>
+                          Validator.validateName(value ?? "", context),
+                      controller: nameController,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: AppLocalizations.of(context)!.username,
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    TextFormField(
+                      validator: (value) =>
+                          Validator.validatePhoneNumber(value ?? "", context),
+                      controller: phoneController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        hintText: AppLocalizations.of(context)!.phonenumber,
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    TextFormField(
+                      validator: (value) =>
+                          Validator.validateEmail(value ?? "", context),
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    TextFormField(
+                      obscureText: _showPassword,
+                      validator: (value) => Validator.validatePasswordRegister(
+                          value ?? "", secondpasswordController.text, context),
+                      controller: firstpasswordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        hintText: AppLocalizations.of(context)!.password,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _showPassword = !_showPassword;
+                            });
+                          },
+                          child: Icon(
+                            _showPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            //color: Colors.grey,
+                          ),
+                        ),
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    TextFormField(
+                      obscureText: _showPassword,
+                      validator: (value) => Validator.validatePasswordRegister(
+                          value ?? "", firstpasswordController.text, context),
+                      controller: secondpasswordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        hintText: AppLocalizations.of(context)!.password,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _showPassword = !_showPassword;
+                            });
+                          },
+                          child: Icon(
+                            _showPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            //color: Colors.grey,
+                          ),
+                        ),
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.06),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: registerUsers,
+                        child: Text(
+                          AppLocalizations.of(context)!.create,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
