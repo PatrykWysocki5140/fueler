bf85d5728892ed3eb432da97f0773e6f1ceafd8f
PatrykWysocki5140 <estu@onet.pl>
2022-12-10 16:18:27 +0100

    add admin page view

---
+++ lib/routes/UI/user_admin_screen/admin_screen.dart
@@ -0,0 +1,128 @@
+import 'dart:developer';
+import 'dart:ui';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import 'package:fueler/routes/UI/user_admin_screen/widgets/users_list_widget.dart';
+import 'package:fueler/settings/Get_colors.dart';
+
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../model/API_Model/User.dart';
+import '../../../model/API_Model/UserPrivilegeLevel.dart';
+import '../../../notifiers/APINotifier.dart';
+import '../../../settings/Get_colors.dart';
+import '../../../settings/validator.dart';
+
+class AdminScreen extends StatefulWidget {
+  static String id = "user_screen";
+  const AdminScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Provider.of<Api>(context).getAllUsers();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: size.height * 0.12),
+          Text(
+            AppLocalizations.of(context)!.adminpanel,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("/profile/user");
+              },
+              child: Text(
+                AppLocalizations.of(context)!.meprofile,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("/profile/createnewuser");
+              },
+              child: Text(
+                AppLocalizations.of(context)!.createnewuser,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          Center(
+            child: Text(
+              Provider.of<Api>(context).users.isEmpty == true
+                  ? AppLocalizations.of(context)!.ifuserslistisemptyerror
+                  : AppLocalizations.of(context)!.searchtoedit,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: UserListScreen(),
+          ),
+        ],
+      ),
+    ));
+
+    // implement the list view
+
+/*
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: objects.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(objects[index]),
+              // Po kliknięciu w element listy wyświetl szczegóły obiektu
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(objects[index]['phoneNumber']),
+                    );
+                  },
+                );
+              },
+            );
+          },
+        ),
+      );*/
+  }
+}
