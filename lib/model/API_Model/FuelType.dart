adca368eaac3e673f0dfecee7ce8bfeb7e2e11cc
PatrykWysocki5140 <estu@onet.pl>
2022-12-11 17:52:54 +0100

    enum FuelType

---
+++ lib/model/API_Model/FuelType.dart
@@ -0,0 +1,13 @@
+// ignore: file_names
+// ignore_for_file: constant_identifier_names
+
+enum FuelType {
+  GASOLINE95,
+  GASOLINE98,
+  GASOLINE_PREMIUM,
+  DIESEL,
+  DIESEL_PREMIUM,
+  LPG,
+  CNG,
+  UNDEFINED
+}
