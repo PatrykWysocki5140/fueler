b4a8fd27f7bacbce52fc116ff8ea7230a8b0e328
PatrykWysocki5140 <estu@onet.pl>
2022-12-11 14:51:17 +0100

    Card style

--- lib/style/styles.dart
+++ lib/style/styles.dart
@@ -8,135 +8,143 @@ import '../settings/Get_colors.dart';
 class Styles {
   static ThemeData themeData(bool isDarkTheme) {
     return ThemeData(
-      // Nagłówek
-      appBarTheme: AppBarTheme(
-        backgroundColor:
-            isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
-        foregroundColor:
-            isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
-      ),
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// Przciski
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor:
-            isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
-        foregroundColor:
-            isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
-        hoverColor:
-            isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        // Nagłówek
+        appBarTheme: AppBarTheme(
           backgroundColor:
               isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
           foregroundColor:
               isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
         ),
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// Przciski
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor:
+              isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
           foregroundColor:
               isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
-        ),
-      ),
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// tło dolengo menu - nieaktywne
-      //canvasColor: isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// Dolne menu
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor:
-            isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
-        selectedItemColor:
-            isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-        unselectedItemColor:
-            isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
-      ),
-
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// style konkretnych stron
-      scaffoldBackgroundColor:
-          isDarkTheme ? GetColors.darkShades : GetColors.lightShades,
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// styl tekstów - globalny
-      textSelectionTheme: TextSelectionThemeData(
-        cursorColor:
-            isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-        selectionColor:
-            isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-        selectionHandleColor:
-            isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-      ),
-      textTheme: TextTheme(
-          bodyText1: TextStyle(
-              color: isDarkTheme
-                  ? GetColors.darkMainFont
-                  : GetColors.lightMainFont),
-          bodyText2: TextStyle(
-              color: isDarkTheme
-                  ? GetColors.darkMainFont
-                  : GetColors.lightMainFont),
-          button: TextStyle(
-              color: isDarkTheme
-                  ? GetColors.darkMainFont
-                  : GetColors.lightMainFont),
-          subtitle1: TextStyle(
-              color: isDarkTheme
-                  ? GetColors.darkMainFont
-                  : GetColors.lightMainFont),
-          subtitle2: TextStyle(
-              color: isDarkTheme
-                  ? GetColors.darkMainFont
-                  : GetColors.lightMainFont)),
-
-      ////////////////////////////////////////////////////////////////////
-      ///
-      /// styl formularza
-
-      inputDecorationTheme: InputDecorationTheme(
-        errorStyle: TextStyle(
-          color: GetColors.error,
-        ),
-        hintStyle: TextStyle(
-          color:
+          hoverColor:
               isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
         ),
-        labelStyle: TextStyle(
-          color: isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100)),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            backgroundColor:
+                isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
+            foregroundColor:
+                isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
+          ),
         ),
-        suffixIconColor: GetColors.error,
-        // isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
-        filled: true,
-        fillColor: isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
-        focusedBorder: OutlineInputBorder(
-          //borderRadius: BorderRadius.circular(25.0),
-          borderSide: BorderSide(width: 3, color: GetColors.warning),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100)),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            foregroundColor:
+                isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
+          ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.0),
-          borderSide: BorderSide(
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// tło dolengo menu - nieaktywne
+        //canvasColor: isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// Dolne menu
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor:
+              isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
+          selectedItemColor:
+              isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
+          unselectedItemColor:
+              isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
+        ),
+
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// style konkretnych stron
+        scaffoldBackgroundColor:
+            isDarkTheme ? GetColors.darkShades : GetColors.lightShades,
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// styl tekstów - globalny
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor:
+              isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
+          selectionColor:
+              isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
+          selectionHandleColor:
+              isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
+        ),
+        textTheme: TextTheme(
+            bodyText1: TextStyle(
+                color: isDarkTheme
+                    ? GetColors.darkMainFont
+                    : GetColors.lightMainFont),
+            bodyText2: TextStyle(
+                color: isDarkTheme
+                    ? GetColors.darkMainFont
+                    : GetColors.lightMainFont),
+            button: TextStyle(
+                color: isDarkTheme
+                    ? GetColors.darkMainFont
+                    : GetColors.lightMainFont),
+            subtitle1: TextStyle(
+                color: isDarkTheme
+                    ? GetColors.darkMainFont
+                    : GetColors.lightMainFont),
+            subtitle2: TextStyle(
+                color: isDarkTheme
+                    ? GetColors.darkMainFont
+                    : GetColors.lightMainFont)),
+
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// styl formularza
+
+        inputDecorationTheme: InputDecorationTheme(
+          errorStyle: TextStyle(
+            color: GetColors.error,
+          ),
+          hintStyle: TextStyle(
             color: isDarkTheme
                 ? GetColors.mainColorDark
                 : GetColors.mainColorLight,
-            width: 2.0,
+          ),
+          labelStyle: TextStyle(
+            color:
+                isDarkTheme ? GetColors.darkMainFont : GetColors.lightMainFont,
+          ),
+          suffixIconColor: GetColors.error,
+          // isDarkTheme ? GetColors.mainColorDark : GetColors.mainColorLight,
+          filled: true,
+          fillColor: isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
+          focusedBorder: OutlineInputBorder(
+            //borderRadius: BorderRadius.circular(25.0),
+            borderSide: BorderSide(width: 3, color: GetColors.warning),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(
+              color: isDarkTheme
+                  ? GetColors.mainColorDark
+                  : GetColors.mainColorLight,
+              width: 2.0,
+            ),
           ),
         ),
-      ),
+        ////////////////////////////////////////////////////////////////////
+        ///
+        /// styl listy
 
-      ////////////////////////////////////////////////////////////////////
-      ///
-      ///
+        cardTheme: CardTheme(
+          color: isDarkTheme ? GetColors.darkAccent : GetColors.lightAccent,
+        )
+        ////////////////////////////////////////////////////////////////////
+        ///
+        ///
 /*
         hoverColor: isDarkTheme ? GetColors.black : GetColors.red,
         appBarTheme: AppBarTheme(
@@ -186,6 +194,6 @@ class Styles {
             ),
           ),
         )*/
-    );
+        );
   }
 }
