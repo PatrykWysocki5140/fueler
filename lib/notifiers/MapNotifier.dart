b85e3ce9bce7a95a6233085fc06e9bb16d0ecb2d
PatrykWysocki5140 <estu@onet.pl>
2022-12-29 13:24:57 +0100

    add getAddressFromLatLng

--- lib/notifiers/MapNotifier.dart
+++ lib/notifiers/MapNotifier.dart
@@ -1,18 +1,12 @@
 // ignore: file_names
 import 'dart:convert';
-import 'dart:developer' as dev;
-import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'dart:developer';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/rendering.dart';
-import 'package:fueler/model/API_Model/PriceEntries.dart';
-import 'package:fueler/model/API_Model/User.dart';
-import 'package:fueler/model/API_Model/UserPrivilegeLevel.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../model/API_Model/Api_service.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleMaps with ChangeNotifier {
   //String baseUrl = "http://fueler.data.awrzawinski.xyz";
@@ -22,6 +16,7 @@ class GoogleMaps with ChangeNotifier {
   String preferencesKeyNightMode = "dark_mode_enabled";
   String mapTheme = "";
   late bool isDarkTheme;
+  String mapApiKey = "AIzaSyBgTRaSwoKFHa5-pFg6JJOgR5VPw6Cb8Ds";
 
   setMapTheme() async {
     final prefs = await SharedPreferences.getInstance();
@@ -53,4 +48,24 @@ class GoogleMaps with ChangeNotifier {
       return false;
     }
   }
+
+//// nie działa
+  Future<String?> getAddressFromLatLng(double lat, double lng) async {
+    String _host = 'https://maps.google.com/maps/api/geocode/json';
+    //final url = '$_host?key=$mapApiKey&language=en&latlng=$lat,$lng';
+    final url = '$_host?latlng=$lat,$lng&key=$mapApiKey';
+    if (lat != null && lng != null) {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Map data = jsonDecode(response.body);
+        String _formattedAddress = data["results"][0]["formatted_address"];
+        log("Adres: $_formattedAddress");
+        return _formattedAddress;
+      } else
+        return null;
+    } else
+      return null;
+  }
+
+  /////////
 }
