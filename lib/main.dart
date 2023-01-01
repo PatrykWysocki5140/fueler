ef831ba2e7b628e942fced3d5198a61bd08bfc24
PatrykWysocki5140 <estu@onet.pl>
2023-01-01 21:59:46 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,11 @@ import 'notifiers/auth.dart';
 import 'dart:developer';
 import 'routes/UI/splash_screen/splash_screen_page.dart';
 
-//late final BuildContext Parentcontext;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   log("start");
   final AuthModel _auth = AuthModel();
   final Api api = Api();
-  //api.GetLocalUser();
-  //log("id:" + api.user.id.toString());
-  //_auth.loadSettings();
-  /*
-  try {
-  } catch (e) {
-    log("Error Loading User Settings: $e");
-  }*/
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => LanguageNotifier()),
@@ -42,19 +33,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-/*
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      onGenerateRoute: routes,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-    );
-  }*/
-
   @override
   Widget build(BuildContext context) => Consumer<Api>(
       builder: (context, apimodel, child) => Consumer<GoogleMaps>(
@@ -68,9 +46,6 @@ class MyApp extends StatelessWidget {
                                 initialData: Styles.themeData(false),
                                 builder: (BuildContext context,
                                     AsyncSnapshot<ThemeData> themeData) {
-                                  //const SplashScreen("/main");
-                                  //Parentcontext = context;
-                                  //setContext(context);
                                   Provider.of<Api>(context).GetLocalUser();
 
                                   return MaterialApp(
@@ -88,47 +63,22 @@ class MyApp extends StatelessWidget {
                                         .map((e) => Locale(e.key)),
                                     locale: Locale(languages.language.key),
                                     initialRoute: '/',
-                                    //home: child ?? const SizedBox.shrink()
-                                    home:
-                                        /*
-                          Consumer<AuthModel>(builder: (context, model, child) {
-                        if (model.user != null) return SplashScreen("/main");
-                        return SplashScreen("/main");
-                      })*/
-                                        const SplashScreen("/main"),
+                                    home: const SplashScreen("/main"),
                                   );
                                 }),
-                        //child: const MyHomePage(title: '')
                       )))));
 }
 
-/*
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Root();
-  }
-}
-*/
 Route<dynamic> routes(RouteSettings settings) {
   log("routes" + settings.name.toString());
   switch (settings.name) {
     case '/':
       return MaterialPageRoute(
         builder: (_) => const MainPageMenu(routesBuilder: subRoutes),
-        // builder: (_) { const MainPageMenu(routesBuilder: subRoutes);},
       );
     case '/main':
       return MaterialPageRoute(
         builder: (_) => const MainPageMenu(routesBuilder: subRoutes),
-        // builder: (_) { const MainPageMenu(routesBuilder: subRoutes);},
       );
     case '/parent':
       return MaterialPageRoute(
