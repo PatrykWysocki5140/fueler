f48ced45faa207662ad918b074223840f9c0e2be
PatrykWysocki5140 <estu@onet.pl>
2023-01-02 18:19:08 +0100

    Update MyJson.dart

--- lib/model/API_Model/MyJson.dart
+++ lib/model/API_Model/MyJson.dart
@@ -12,6 +12,11 @@ class MyJson {
   // ignore: non_constant_identifier_names
   String JsonDecoderList(String json) {
     json = json.replaceAll('"}", "{"', '"}, {"');
+    json = json.replaceAll('"{"longitude": "', '"');
+    json = json.replaceAll('", "latitude": "', ',');
+    json = json.replaceAll('"}", "name":', '", "name":');
+    json = json.replaceAll('": "[{"', '", "": [{"');
+    json = json.replaceAll('"}]"}', '", ""}]}');
     return json;
   }
 }
