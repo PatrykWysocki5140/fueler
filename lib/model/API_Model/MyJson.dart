b5de58efa746812062bfc21f26a2f1719f66c1db
PatrykWysocki5140 <estu@onet.pl>
2023-01-03 21:10:56 +0100

    Update MyJson.dart

--- lib/model/API_Model/MyJson.dart
+++ lib/model/API_Model/MyJson.dart
@@ -12,11 +12,20 @@ class MyJson {
   // ignore: non_constant_identifier_names
   String JsonDecoderList(String json) {
     json = json.replaceAll('"}", "{"', '"}, {"');
+    json = json.replaceAll('"{"latitude": "', '"');
+    // json = json.replaceAll('"{"longitude": "', '"');
+    json = json.replaceAll('", "longitude": "', ',');
+    //json = json.replaceAll('", "latitude": "', ',');
+    json = json.replaceAll('"}", "name":', '", "name":');
+    json = json.replaceAll('": "[{"', '": [{"');
+    json = json.replaceAll('"}]"}', '"}]}');
+    return json;
+  }
+
+  String JsonDecoderCoordinates(String json) {
     json = json.replaceAll('"{"longitude": "', '"');
     json = json.replaceAll('", "latitude": "', ',');
-    json = json.replaceAll('"}", "name":', '", "name":');
-    json = json.replaceAll('": "[{"', '", "": [{"');
-    json = json.replaceAll('"}]"}', '", ""}]}');
+    json = json.replaceAll('"}", "name"', '", "name"');
     return json;
   }
 }
