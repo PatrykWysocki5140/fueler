50887a91532d43dc88d0cf3b3db23e948dc456cc
PatrykWysocki5140 <estu@onet.pl>
2023-01-06 19:32:04 +0100

    Create FuelStationBrands.dart

---
+++ lib/model/API_Model/FuelStationBrands.dart
@@ -0,0 +1,17 @@
+// ignore: file_names
+// ignore_for_file: constant_identifier_names
+
+enum FuelStationBrands {
+  orlen,
+  lotos,
+  shell,
+  bp,
+  intermarche,
+  circlek,
+  auchan,
+  amic,
+  huzar,
+  moya,
+  statoil,
+  def
+}
